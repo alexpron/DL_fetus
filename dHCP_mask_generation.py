060dd1c46ea7b784476a4189790efb82a5a10e96
meritxell <mrieramarin@gmail.com>
2020-05-25 14:00:26 +0200

    Take the 100 youngest targets.

--- dHCP_mask_generation.py
+++ dHCP_mask_generation.py
@@ -1,5 +1,6 @@
 import nibabel as nib
 from os.path import expanduser
+
 home = expanduser("~")
 import os
 
@@ -8,67 +9,100 @@ import numpy as np
 import glob
 import shutil
 from PIL import Image
-#import uuid
+# import uuid
 import string, random
+import csv
 
 # put all the same type of image together in the same directory
 src_directory = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/dhcp_t2_and_seg_data/'
 dst_directory = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/img2D/'
-# dst_dir0 = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/restore_T2w'
-# dst_dir1 = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/drawem87_space-T2w_dseg'
-# dst_dir2 = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/drawem9_space-T2w_dseg'
+# dst_dir0 = home+'/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/restore_T2w'
+# dst_dir1 = home+'/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/drawem87_space-T2w_dseg'
+# dst_dir2 = home+'/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/drawem9_space-T2w_dseg'
 dst_dir3 = home + '/Desktop/UPC/DD/Final_Project/Data_analysis/img_classification/ribbon_space-T2w_dseg'
 
 if not os.path.isdir(dst_directory):
     os.mkdir(dst_directory)
 
 # if not os.path.isdir(dst_dir0):
-#     os.mkdir(dst_dir0)
+#    os.mkdir(dst_dir0)
 
 # if not os.path.isdir(dst_dir1):
 #    os.mkdir(dst_dir1)
 
 # if not os.path.isdir(dst_dir2):
-#     os.mkdir(dst_dir2)
+#    os.mkdir(dst_dir2)
 
 if not os.path.isdir(dst_dir3):
     os.mkdir(dst_dir3)
 
-# ToDo: select the 100 youngest targets
+with open(home+'/Desktop/UPC/DD/Final_Project/Data_analysis/dhcp_t2_and_seg_data/participants.tsv', newline='') as tsvfile:
+    reader = csv.reader(tsvfile, delimiter='\t')
+    data = list(reader)
+
+variables = data[0]
+data.pop(0)
+birth_date = []
+
+for row in data:
+    vect = [row[0], row[2]]  # ["id", "birth_date"]
+    birth_date.append(vect)
+
+
+def take_second(elem):
+    return elem[1]
+
+
+birth_date_s = sorted(birth_date, key=take_second)
+birth_date_100 = []
+# count_100 = 0
+
+for i in range(len(birth_date_s)):
+    if float(birth_date_s[i][1]) < 36:
+        # count_100 += 1
+        birth_date_100.append(birth_date_s[i])
+
 src_subjects = glob.glob(src_directory + 'sub-*', recursive=True)
-for subject in src_subjects:
-    src_sessions = glob.glob(subject + '/ses-*', recursive=True)
-    for session in src_sessions:
-        # src_t2w = glob.glob(session + '/*-restore_T2w.nii.gz')[0]
-        # shutil.copy2(src_t2w, dst_dir0)
+num_cortex = 0
 
-        # src_drawem87 = glob.glob(session + '/*-drawem87_space-T2w_dseg.nii.gz')[0]
-        # shutil.copy2(src_drawem87, dst_dir1)
+while num_cortex <= 100:
+    for subject in src_subjects:
+        sub = subject.split('/')[9].split('-')[1]
 
-        # src_drawem9 = glob.glob(session + '/*-drawem9_space-T2w_dseg.nii.gz')[0]
-        # shutil.copy2(src_drawem9, dst_dir2)
+        # select the subject that are in the list of the 100 youngest
+        if any(sub in s for s in birth_date_100):
+            src_sessions = glob.glob(subject + '/ses-*', recursive=True)
 
-        src_cortex = glob.glob(session + '/*-ribbon_space-T2w_dseg.nii.gz')
-        if len(src_cortex) == 1:
-            shutil.copy2(src_cortex[0], dst_dir3)
+            for session in src_sessions:
+                # src_t2w = glob.glob(session+'/*-restore_T2w.nii.gz')[0]
+                # shutil.copy2(src_t2w, dst_dir0)
 
-# img_T2w = glob.glob(dst_dir0 + '/*-restore_T2w.nii.gz', recursive=True)
-# img_drawem87 = glob.glob(dst_dir1 + '/*-drawem87_space-T2w_dseg.nii.gz', recursive=True)
-# img_drawem9 = glob.glob(dst_dir2 + '/*-drawem9_space-T2w_dseg.nii.gz', recursive=True)
+                # src_drawem87 = glob.glob(session+'/*-drawem87_space-T2w_dseg.nii.gz')[0]
+                # shutil.copy2(src_drawem87, dst_dir1)
+
+                # src_drawem9 = glob.glob(session+'/*-drawem9_space-T2w_dseg.nii.gz')[0]
+                # shutil.copy2(src_drawem9, dst_dir2)
+
+                src_cortex = glob.glob(session + '/*-ribbon_space-T2w_dseg.nii.gz')
+                if len(src_cortex) == 1:
+                    shutil.copy2(src_cortex[0], dst_dir3)
+                    num_cortex += 1
+
+# img_T2w = glob.glob(dst_dir0+'/*-restore_T2w.nii.gz', recursive=True)
+# img_drawem87 = glob.glob(dst_dir1+'/*-drawem87_space-T2w_dseg.nii.gz', recursive=True)
+# img_drawem9 = glob.glob(dst_dir2+'/*-drawem9_space-T2w_dseg.nii.gz', recursive=True)
 img_cortex = glob.glob(dst_dir3 + '/*-ribbon_space-T2w_dseg.nii.gz', recursive=True)
 
 count = 0
-# img0, img1, img2, img3 = []
-num_imgs = 100  # len(img_restore_T2w)
+num_imgs = 100  # len(img_cortex)
 
 # indicate for which directory we want to generate masks
 dirr = img_cortex
 
-for j in range(num_imgs):
+for j in range(len(dirr)):
     if count == num_imgs:
         break
     else:
-        count += 1
         img0 = nib.load(dirr[j])
         data0 = img0.get_fdata()
         mask0 = np.random.rand(*data0.shape)
@@ -89,4 +123,7 @@ for j in range(num_imgs):
         mask_img.save(dst_directory + 'ribbon_space-T2w_dseg/mask/' + str(id_) + '_' + str(j) + '.jpg')
 
         # plt.imsave(dst_directory+'/mask/data'+str(j)+'.jpg', final_m)
-        plt.imsave(dst_directory + 'ribbon_space-T2w_dseg/imgs/' + str(id_) + '_' + str(j) + '.jpg', final_d)  # cmap = 'gray'
+        plt.imsave(dst_directory + 'ribbon_space-T2w_dseg/imgs/' + str(id_) + '_' + str(j) + '.jpg',
+                   final_d)  # cmap = 'gray'
+
+        count += 1
